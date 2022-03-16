4be25c2b85b962957f70182c4626f501205a5e15
andres lopez <andresfelipelg@gmail.com>
2022-03-16 11:43:06 -0500

    realizan implementacion del proyecto

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/admin', function () {
+    return view('auth.login');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
