1ad5ddab4aa59ed234ad601d9c0cce2e75dc6841
Andres Lopez <andresfelipelg@gmail.com>
2022-04-06 05:55:37 -0500

    organizando roles edit

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -12,22 +12,54 @@
 
     <div class="col-4 pt-5 m-auto ">
         <div class="text-center mt-5 mb-5 ">
-    <form  action="{{route('permissions.update',$permission->id) }}" method="POST" class="mb-5">
+    <form  action="{{route('roles.update',$role->id) }}" method="POST" class="mb-5">
         @csrf
         @method('PUT')
         <i class=" display-2 text-white bi bi-person-circle "></i>
         <h1 class= "mt-4 h2 font-weight-normal text-white" >Editar Rol</h1>
 
         <div class="mb-3 mt-3 text-start">
-            <input class="form-control" name="name"
-            value="{{ $permission->name }}" type="text" placeholder="Ingrese el nombre del permiso">
+            <input class="form-control" name="name" value="{{ $role->name }}" type="text" placeholder="Ingrese el nombre del role">
        </div>
 
-
+       <div class="row">
+        <label for="name" class="col-sm-2 col-form-label text-white">Permisos</label>
+        <div class="col-sm-12">
+          <div class="form-group">
+            <div class="tab-content">
+              <div class="tab-pane active">
+                <table class="table">
+                  <tbody>
+                    @foreach ($permissions as $id => $permission)
+                    <tr>
+                      <td>
+                        <div class="form-check">
+                          <label class="form-check-label">
+                            <input class=" form-check-input" type="checkbox" name="permissions[]"
+                              value="{{ $id }}" {{ $role->permissions->contains($id) ? 'checked' : '' }}>
+                            <span class=" form-check-sign">
+                              <span class="check"></span>
+                            </span>
+                          </label>
+                        </div>
+                      </td>
+                      <td class="text-white">
+                        {{ $permission }}
+                      </td>
+                    </tr>
+                  @endforeach
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <div class="mb-2 d-flex">
         <button class="mt-2 btn btn-dark px-4 py-2" type="submit"><i class="bi bi-send-plus-fill"></i> Guardar</button>
-        <a href="{{ route('permissions.index')}}" class="mt-2 btn btn-secondary text-white btn-outline-dark ms-2 px-4 py-2"><i class="  bi bi-arrow-left-circle"></i> Atras</a>
+        <a href="{{ route('roles.index')}}" class="mt-2 btn btn-secondary text-white btn-outline-dark ms-2 px-4 py-2"><i class="  bi bi-arrow-left-circle"></i> Atras</a>
 
     </div>
     </form>
